302fb46121b73886787971682c5dee6cf666d895
Cyber-Globe <ojeikelvin@gmail.com>
2023-07-04 10:24:07 -0700

    task done

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,2 +1,6 @@
 class LockedClass:
-    __slots__ = ['first_name']
+    def __setattr__(self, name, value):
+        if hasattr(self, name) and name != 'first_name':
+            raise AttributeError("'LockedClass' object cannot create new attributes")
+        super().__setattr__(name, value)
+
